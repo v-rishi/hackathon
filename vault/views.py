b8a1dcf2dfa442331d9630acde54f839218cfba6
rishi <rishi@yodaplus.com>
2023-04-29 15:57:23 +0530

    fixed bug

--- vault/views.py
+++ vault/views.py
@@ -25,7 +25,6 @@ class CreatePasswordView(LoginRequiredMixin, CreateView):
     form_class = CreatePasswordForm
     success_url = reverse_lazy('vault:home_page_view')
     
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['user'] = self.request.user
-        return kwargs
+    def form_valid(self, form):
+        form.instance.user = self.request.user 
+        return super().form_valid(form)
